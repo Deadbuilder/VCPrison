dd881ca656833f432a3c3f6d6d47963b1ea5c92d
Nicholas <tacticalsk8er@gmail.com>
2014-07-30 21:44:26 -0700

    Init setup

---
+++ src/main/java/net/vaultcraft/vcprison/VCPrison.java
@@ -0,0 +1,19 @@
+package net.vaultcraft.vcprison;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by tacticalsk8er on 7/30/2014.
+ */
+public class VCPrison extends JavaPlugin {
+
+    private static VCPrison instance;
+
+    public void onEnable() {
+        instance = this;
+    }
+
+    public static VCPrison getInstance() {
+        return instance;
+    }
+}
