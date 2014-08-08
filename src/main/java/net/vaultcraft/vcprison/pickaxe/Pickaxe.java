6312ca76cda09caf6fbca571aed8401a20a10aa6
Nicholas <tacticalsk8er@gmail.com>
2014-08-08 15:23:49 -0700

    Level Changes

--- src/main/java/net/vaultcraft/vcprison/pickaxe/Pickaxe.java
+++ src/main/java/net/vaultcraft/vcprison/pickaxe/Pickaxe.java
@@ -119,9 +119,9 @@ public class Pickaxe {
 
     public int toNextLevel(int level) {
         if (level < 50)
-            return (600 + (600 * (level - 1))) / 100;
+            return (600 + (600 * (level - 1)));
         else
-            return 30000 / 10000;
+            return 30000;
     }
 
     public void mineBlock(Material material) {
