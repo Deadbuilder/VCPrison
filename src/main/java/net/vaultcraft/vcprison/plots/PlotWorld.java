27f2726c145ade2980fe12c046a555e1f043c2c1
Sean CyberKitsune McClenaghan <cyberkitsune09@gmail.com>
2014-10-10 23:58:16 -0700

    Add newly generated plots (hopefully!)

--- src/main/java/net/vaultcraft/vcprison/plots/PlotWorld.java
+++ src/main/java/net/vaultcraft/vcprison/plots/PlotWorld.java
@@ -1,5 +1,6 @@
 package net.vaultcraft.vcprison.plots;
 
+import net.vaultcraft.vcprison.VCPrison;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.WorldCreator;
@@ -12,10 +13,11 @@ public class PlotWorld {
     private static World plotWorld;
     private static PlotManager plotManager;
     private static PlotMenu plotMenu;
+    private static PlotGen plotGenerator;
 
     public PlotWorld() {
         WorldCreator wc = new WorldCreator(PlotInfo.worldName);
-        PlotGen plotGenerator = new PlotGen();
+
         wc.generator(plotGenerator);
         wc.generateStructures(false);
         plotManager = new PlotManager();
@@ -23,6 +25,7 @@ public class PlotWorld {
         plotWorld = Bukkit.getServer().createWorld(wc);
         plotGenerator.addGeneratedPlots();
         plotManager.generatePlots();
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(VCPrison.getInstance(), plotGenerator::addGeneratedPlots, 0, 20);
     }
 
     public static World getPlotWorld() {
