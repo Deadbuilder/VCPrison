1a5d68a65cfea454c808ab5d2ace6da6dba0fe54
Kai Mills <craftfestnetwork@gmail.com>
2014-10-21 14:54:26 -0600

    Updated temporary kits and changed WARDEN to GUARD.

--- src/main/java/net/vaultcraft/vcprison/commands/VCKit.java
+++ src/main/java/net/vaultcraft/vcprison/commands/VCKit.java
@@ -4,9 +4,7 @@ import net.vaultcraft.vcutils.chat.Form;
 import net.vaultcraft.vcutils.chat.Prefix;
 import net.vaultcraft.vcutils.command.ICommand;
 import net.vaultcraft.vcutils.user.Group;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 /**
@@ -20,17 +18,33 @@ public class VCKit extends ICommand {
 
     public void processCommand(Player player, String[] args) {
         if(args.length == 0) {
-            player.sendMessage(ChatColor.LIGHT_PURPLE + "Available kits" + ChatColor.GRAY + ":");
-            player.sendMessage(ChatColor.GRAY + "- " + ChatColor.GRAY + "Common");
-            player.sendMessage(ChatColor.GRAY + "- " + ChatColor.DARK_GRAY + "Wolf");
-            player.sendMessage(ChatColor.GRAY + "- " + ChatColor.GREEN + "Slime");
-            player.sendMessage(ChatColor.GRAY + "- " + ChatColor.WHITE + "Skeleton");
-            player.sendMessage(ChatColor.GRAY + "- " + ChatColor.DARK_PURPLE + "Enderman");
-            player.sendMessage(ChatColor.GRAY + "- " + ChatColor.YELLOW + "Wither");
-            player.sendMessage(ChatColor.GRAY + "- " + ChatColor.DARK_PURPLE + "Ender" + ChatColor.GRAY + "Dragon");
-            player.sendMessage(ChatColor.GRAY + "/kit <name>");
+            player.sendMessage(ChatColor.DARK_PURPLE + "Available kits" + ChatColor.GRAY + ":");
+            player.sendMessage(ChatColor.GRAY + "- " + ChatColor.LIGHT_PURPLE + "Common");
+            player.sendMessage(ChatColor.GRAY + "- " + ChatColor.LIGHT_PURPLE + "Wolf");
+            player.sendMessage(ChatColor.GRAY + "- " + ChatColor.LIGHT_PURPLE + "Slime");
+            player.sendMessage(ChatColor.GRAY + "- " + ChatColor.LIGHT_PURPLE + "Skeleton");
+            player.sendMessage(ChatColor.GRAY + "- " + ChatColor.LIGHT_PURPLE + "Enderman");
+            player.sendMessage(ChatColor.GRAY + "- " + ChatColor.LIGHT_PURPLE + "Wither");
+            player.sendMessage(ChatColor.GRAY + "- " + ChatColor.LIGHT_PURPLE + "EnderDragon");
+            player.sendMessage(ChatColor.GRAY + "Usage: /kit <kit name>");
         } else if(args.length == 1) {
-            Form.at(player, Prefix.ERROR, "Kits are not yet released. Coming soon!");
+            if(args[1].equalsIgnoreCase("common")) {
+                Form.at(player, Prefix.SUCCESS, "You spawned the kit: " + ChatColor.LIGHT_PURPLE + "Common");
+            } else if(args[1].equalsIgnoreCase("wolf")) {
+                Form.at(player, Prefix.SUCCESS, "You spawned the kit: " + ChatColor.LIGHT_PURPLE + "Wolf");
+            } else if(args[1].equalsIgnoreCase("slime")) {
+                Form.at(player, Prefix.SUCCESS, "You spawned the kit: " + ChatColor.LIGHT_PURPLE + "Slime");
+            } else if(args[1].equalsIgnoreCase("skeleton")) {
+                Form.at(player, Prefix.SUCCESS, "You spawned the kit: " + ChatColor.LIGHT_PURPLE + "Skeleton");
+            } else if(args[1].equalsIgnoreCase("enderman")) {
+                Form.at(player, Prefix.SUCCESS, "You spawned the kit: " + ChatColor.LIGHT_PURPLE + "Enderman");
+            } else if(args[1].equalsIgnoreCase("wither")) {
+                Form.at(player, Prefix.SUCCESS, "You spawned the kit: " + ChatColor.LIGHT_PURPLE + "Wither");
+            } else if(args[1].equalsIgnoreCase("enderdragon")) {
+                Form.at(player, Prefix.SUCCESS, "You spawned the kit: " + ChatColor.LIGHT_PURPLE + "EnderDragon");
+            } else {
+                Form.at(player, Prefix.ERROR, "That kit does not exist.");
+            }
         } else if(args.length > 1) {
             Form.at(player, Prefix.ERROR, "Too many arguments.");
         }
