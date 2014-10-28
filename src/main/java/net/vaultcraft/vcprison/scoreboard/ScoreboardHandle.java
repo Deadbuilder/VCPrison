1599eb12f632e9e0c4abaeefa10090d76f06245e
Connor Hollasch <connor@hollasch.net>
2014-10-27 23:49:45 -0700

    Scoreboard things

--- src/main/java/net/vaultcraft/vcprison/scoreboard/ScoreboardHandle.java
+++ src/main/java/net/vaultcraft/vcprison/scoreboard/ScoreboardHandle.java
@@ -10,6 +10,7 @@ import net.vaultcraft.vcutils.scoreboard.VCDisplay;
 import net.vaultcraft.vcutils.scoreboard.VCObjective;
 import net.vaultcraft.vcutils.scoreboard.VCScore;
 import net.vaultcraft.vcutils.scoreboard.VCScoreboard;
+import net.vaultcraft.vcutils.user.User;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
@@ -29,16 +30,19 @@ public class ScoreboardHandle implements Runnable {
     public ScoreboardHandle(Player player) {
         board = new VCScoreboard(player);
         this.player = player;
-        text.put(12, " ");
-        text.put(11, "&5Current Rank");
-        text.put(10, "&7{rank}");
-        text.put(9, "  ");
-        text.put(8, "&5Next DP");
-        text.put(7, "&7{dp}");
-        text.put(6, "   ");
-        text.put(5, "&5Rankup Cost");
-        text.put(4, "&7${next}");
-        text.put(3, "    ");
+        text.put(15, " ");
+        text.put(14, "&5Current Rank");
+        text.put(13, "&7{rank}");
+        text.put(12, "  ");
+        text.put(11, "&5Drop Party In");
+        text.put(10, "&7{dp}");
+        text.put(9, "   ");
+        text.put(8, "&5Balance");
+        text.put(7, "&7${bal}");
+        text.put(6, "    ");
+        text.put(5, "&5Next Rank");
+        text.put(4, "&7{next}");
+        text.put(3, "     ");
         text.put(2, "&5Players In FFA");
         text.put(1, "&7{ffa}");
     }
@@ -63,10 +67,35 @@ public class ScoreboardHandle implements Runnable {
 
             use = use.replace("{rank}", user.getRank().toString());
             use = use.replace("{dp}", VCDropParty.MMSS(DropParty.getInstance().getTimeLeft())+"");
-            use = use.replace("{next}", Form.at(Rank.next(user.getRank()).getCost(), true));
+            use = use.replace("{bal}", Form.at(user.getUser().getMoney(), true));
+            use = use.replace("{next}", percent(user.getPlayer()));
             use = use.replace("{ffa}", VCPrison.getFFA().size()+"");
 
             score.setName(ChatColor.translateAlternateColorCodes('&', use));
         }
+    }
+
+    private static String percent(Player player) {
+        PrisonUser pu = PrisonUser.fromPlayer(player);
+        User u = pu.getUser();
+
+        double has = u.getMoney();
+        double needs = Rank.next(pu.getRank()).getCost();
+
+        double left = needs - has;
+
+        int percent = (int)((left / needs)*5);
+        String build = "&5";
+        for (int x = 0; x < percent; x++) {
+            build+="▋";
+        }
+
+        build+="&7";
+        for (int x = 0; x < (5 - percent); x++) {
+            build+="▋";
+        }
+
+        build+=(((left / needs) * 100)+"%");
+        return build;
     }
 }
