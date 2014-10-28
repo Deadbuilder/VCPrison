c6918ffcd32f90759a4e0efaaaecb7c1a8b57a98
Sean McClenaghan <cyberkitsune09@gmail.com>
2014-10-27 18:00:46 -0700

    Bad idea lol

--- src/main/java/net/vaultcraft/vcprison/cells/CellManager.java
+++ src/main/java/net/vaultcraft/vcprison/cells/CellManager.java
@@ -18,33 +18,28 @@ import java.util.UUID;
 public class CellManager {
 
     private World plotWorld;
-    private HashMap<PrisonUser, List<Cell>> allTheCells = new HashMap<>();
-
     public CellManager(World plotWorld) {
         this.plotWorld = plotWorld;
     }
 
     public List<Cell> getCellsFromPlayer(Player player) {
         PrisonUser user = PrisonUser.fromPlayer(player);
-        if(!allTheCells.containsKey(user)) {
-            List<DBObject> objects = VCUtils.getInstance().getMongoDB().queryMutiple(VCUtils.mongoDBName, "Cells", "OwnerUUID", player.getUniqueId().toString());
-            ArrayList<Cell> cells = new ArrayList<>();
-            for (DBObject o : objects) {
-                Cell cell = new Cell();
-                cell.ownerUUID = UUID.fromString((String) o.get("OwnerUUID"));
-                String[] chunkString = ((String) o.get("Chunk")).split(",");
-                cell.chunkX = Integer.parseInt(chunkString[0]);
-                cell.chunkZ = Integer.parseInt(chunkString[1]);
-                for (String s : ((String) o.get("Members")).split(",")) {
-                    cell.additionalUUIDs.add(UUID.fromString(s));
-                }
-                cell.name = (String) o.get("Name");
-                cell.cellSpawn = stringToLocation((String) o.get("SpawnPoint"));
-            }
-            allTheCells.put(user, cells);
-        }
 
-        return allTheCells.get(user);
+        List<DBObject> objects = VCUtils.getInstance().getMongoDB().queryMutiple(VCUtils.mongoDBName, "Cells", "OwnerUUID", player.getUniqueId().toString());
+        ArrayList<Cell> cells = new ArrayList<>();
+        for (DBObject o : objects) {
+            Cell cell = new Cell();
+            cell.ownerUUID = UUID.fromString((String) o.get("OwnerUUID"));
+            String[] chunkString = ((String) o.get("Chunk")).split(",");
+            cell.chunkX = Integer.parseInt(chunkString[0]);
+            cell.chunkZ = Integer.parseInt(chunkString[1]);
+            for (String s : ((String) o.get("Members")).split(",")) {
+                cell.additionalUUIDs.add(UUID.fromString(s));
+            }
+            cell.name = (String) o.get("Name");
+            cell.cellSpawn = stringToLocation((String) o.get("SpawnPoint"));
+        }
+        return cells;
     }
 
     public Cell getCellFromLocation(Location location) {
