8697871cfadbbc0ea351a45074d6cda6a164807f
Nicholas <tacticalsk8er@gmail.com>
2014-11-03 00:21:58 -0800

    Disabled candy

--- src/main/java/net/vaultcraft/vcprison/VCPrison.java
+++ src/main/java/net/vaultcraft/vcprison/VCPrison.java
@@ -1,7 +1,6 @@
 package net.vaultcraft.vcprison;
 
 import com.google.common.collect.Lists;
-import net.vaultcraft.vcprison.candy.*;
 import net.vaultcraft.vcprison.cells.CellManager;
 import net.vaultcraft.vcprison.cells.CellsListener;
 import net.vaultcraft.vcprison.cells.VCCell;
@@ -158,17 +157,17 @@ public class VCPrison extends JavaPlugin {
                 rIterator.remove();
         }
 
-        //Cnady
-        new CandyListener();
-        CandyManager.registerCandy("gum", new Gum());
-        CandyManager.registerCandy("butterscotch", new Butterscotch());
-        CandyManager.registerCandy("sugar", new SugarCube());
-        CandyManager.registerCandy("jawbreaker", new Jawbreaker());
-        CandyManager.registerCandy("sourpatch", new SourPatch());
-        CandyManager.registerCandy("warhead", new Warhead());
-        CandyManager.registerCandy("candyapple", new CandyApple());
-        CandyManager.registerCandy("chocolatebar", new ChocolateBar());
-        CandyManager.registerCandy("cookie", new Cookie());
+        //Candy
+//        new CandyListener();
+//        CandyManager.registerCandy("gum", new Gum());
+//        CandyManager.registerCandy("butterscotch", new Butterscotch());
+//        CandyManager.registerCandy("sugar", new SugarCube());
+//        CandyManager.registerCandy("jawbreaker", new Jawbreaker());
+//        CandyManager.registerCandy("sourpatch", new SourPatch());
+//        CandyManager.registerCandy("warhead", new Warhead());
+//        CandyManager.registerCandy("candyapple", new CandyApple());
+//        CandyManager.registerCandy("chocolatebar", new ChocolateBar());
+//        CandyManager.registerCandy("cookie", new Cookie());
 
         //Add users who are online
         Bukkit.getOnlinePlayers().forEach(net.vaultcraft.vcprison.user.PrisonUser::new);
